f904d7faedb6f8f4eb9b63798a05de3a1edfa220
Ben Gregory <JBGregoryPro@protonmail.com>
2021-07-23 13:08:56 -0500

    reworked pets click event

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,16 +1,45 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [, petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+
+                    let assignedWalker = { name: "" };
+
+                    for (const walker of walkers) {
+                        if (walker.city === parseInt(walkerId)) {
+                            const assignedWalker = pets.filter((walker) => {
+                                if (walker.id === walkerID)
+                                return true
+                            })
+                        }
+                    }
+                    window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
+                }
+            }
+        }
+    }
+)
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
-    
+
     petHTML += "</ul>"
-    
+
     return petHTML
 }
 
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -30,7 +30,8 @@ export const Walkers = () => {
 
         
         
-    }
+    } 
+
     return walkerHTML 
     
 
